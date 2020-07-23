ba0a522e4195d6bfb45abe61c13c30855af134bf
Richard Doe <richard.doe@rwdit.net>
2020-07-23 14:59:39 +0100

    TODOs

--- src/handler.js
+++ src/handler.js
@@ -5,11 +5,14 @@ const constants = require('./constants');
 const europeanaRecordResponse = require('./response');
 
 // TODO: break down into more atomic functions
+// TODO: detect `format` param present and != "json", and return error
 module.exports = async(req, res) => {
   let status;
   let response;
 
   const url = req.query.url;
+  // TODO: consider what other patterns this needs to accept.
+  //       see what youtube, soundcloud, vimeo, etc. do
   const europeanaUriPattern = new RegExp('^http://data.europeana.eu/item(/[0-9]+/[a-zA-Z0-9_]+)$');
   const europeanaUriMatch = url.match(europeanaUriPattern);
   if (europeanaUriMatch) {
