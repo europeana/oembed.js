9d8f8d57991ba78a2345a68fc2fa01d0bbc2eeb5
Richard Doe <richard.doe@rwdit.net>
2020-07-24 12:46:19 +0100

    Limit rich embeds to certain rights statements

---
+++ test/support/fixtures.js
@@ -0,0 +1,20 @@
+module.exports = {
+  rightsStatements: {
+    rich: [
+      'http://creativecommons.org/licenses/by-sa/4.0/',
+      'http://creativecommons.org/licenses/by/4.0/',
+      'http://creativecommons.org/publicdomain/mark/1.0/',
+      'http://creativecommons.org/publicdomain/zero/1.0/'
+    ],
+    link: [
+      'http://creativecommons.org/licenses/by-nc-nd/4.0/',
+      'http://creativecommons.org/licenses/by-nc-sa/4.0/',
+      'http://creativecommons.org/licenses/by-nd/4.0/',
+      'http://rightsstatements.org/vocab/CNE/1.0/',
+      'http://rightsstatements.org/vocab/InC-EDU/1.0/',
+      'http://rightsstatements.org/vocab/InC/1.0/',
+      'http://rightsstatements.org/vocab/NoC-NC/1.0/',
+      'http://rightsstatements.org/vocab/NoC-OKLR/1.0/'
+    ]
+  }
+};
